89f95b8df1a7f159854ba8456e67983b3863f5a6
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-14 11:01:39 +0300

    small refactoring

--- test-task.js
+++ test-task.js
@@ -1,23 +1,29 @@
+// * Functions
 function addInput() {
-  const newId = document.querySelectorAll('input').length;
-  const oldValue = document.getElementById('firstInput').value;
-  
-   
-  if (oldValue === '555') {
+  const newId = document.querySelectorAll('input[type="text"]').length;
+  // input text value
+  const textValue = document.getElementById('firstInput').value;
+
+  // special case
+  if (textValue === '555') {
     return createNewInput('---', newId);
   }
-
-  const newValue = newId % 2 === 0 ? oldValue.toUpperCase() : oldValue.toLowerCase();
-
+  const newValue = isEvenInput(newId, textValue);
   createNewInput(newValue, newId);
 }
 
+// create new text-input
 function createNewInput(value, id) {
-  const form = document.forms.taskForm;
   const newInput = document.createElement('input');
   newInput.type = 'text';
   newInput.value = value;
   newInput.id = id;
+  // append (br + input) couple
+  const form = document.forms.taskForm;
   form.appendChild(document.createElement('br'));
   form.appendChild(newInput);
+}
+
+function isEvenInput(id, textValue) {
+  return id % 2 === 0 ? textValue.toUpperCase() : textValue.toLowerCase();
 }
